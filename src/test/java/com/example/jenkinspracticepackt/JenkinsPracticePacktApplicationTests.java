b3f37564fa9becdcf1ff04dab6fe9319a0da9e6c
Bijan1986 <nayak.bijan@gmail.com>
2023-11-03 20:27:59 +0200

    initial commit

---
+++ src/test/java/com/example/jenkinspracticepackt/JenkinsPracticePacktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinspracticepackt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPracticePacktApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
