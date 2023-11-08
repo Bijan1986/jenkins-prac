865d006f69ba84576e76113cd7856035fdeb7a4c
Bijan1986 <nayak.bijan@gmail.com>
2023-11-08 06:26:15 +0200

    optimised import

--- src/test/java/com/example/jenkinspracticepackt/service/CalculatorTest.java
+++ src/test/java/com/example/jenkinspracticepackt/service/CalculatorTest.java
@@ -2,7 +2,7 @@ package com.example.jenkinspracticepackt.service;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
     private Calculator calculator = new Calculator();
